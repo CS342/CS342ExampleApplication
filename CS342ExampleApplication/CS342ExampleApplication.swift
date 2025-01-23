564b8d7ed1564184f3949618c3890b5ba24748a5
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-01-23 15:44:15 -0800

    Rename Application and Improve Setup

---
+++ CS342ExampleApplication/CS342ExampleApplication.swift
@@ -0,0 +1,37 @@
+//
+// This source file is part of the CS342 Example Application based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import Spezi
+import SpeziFirebaseAccount
+import SpeziViews
+import SwiftUI
+
+
+@main
+struct CS342ExampleApplication: App {
+    @UIApplicationDelegateAdaptor(CS342ExampleApplicationDelegate.self) var appDelegate
+    @AppStorage(StorageKeys.onboardingFlowComplete) var completedOnboardingFlow = false
+
+    
+    var body: some Scene {
+        WindowGroup {
+            ZStack {
+                if completedOnboardingFlow {
+                    HomeView()
+                } else {
+                    EmptyView()
+                }
+            }
+                .sheet(isPresented: !$completedOnboardingFlow) {
+                    OnboardingFlow()
+                }
+                .testingSetup()
+                .spezi(appDelegate)
+        }
+    }
+}
---
+++ CS342ExampleApplicationTests/CS342ExampleApplicationTests.swift
@@ -0,0 +1,18 @@
+//
+// This source file is part of the CS342 Example Application based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import CS342ExampleApplication
+import XCTest
+
+
+class CS342ExampleApplicationTests: XCTestCase {
+    @MainActor
+    func testContactsCount() throws {
+        XCTAssertEqual(Contacts(presentingAccount: .constant(true)).contacts.count, 1)
+    }
+}
